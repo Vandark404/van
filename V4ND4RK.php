ea27c08d02c49174e6d401953a52153cd540059b
Vandark404 <57724301+Vandark404@users.noreply.github.com>
2019-11-13 17:33:33 +0000

    Add files via upload

---
+++ V4ND4RK.php
@@ -0,0 +1,73 @@
+<?php
+
+$username = "Van";
+$password = "Drk";
+
+system('clear');
+echo "\e[0;32mSILAHKAN LOGIN UNTUK MASUK KEDALAM TOOLS INI\n";
+echo "\e[0;32mUSERNAME : ";
+$user = trim(fgets(STDIN));
+echo "\e[0;32mPASSWORD : ";
+$pass = trim(fgets(STDIN));
+if ($user == $username && $pass == $password)
+{
+echo "\e[0;32mLOGIN SUCCES!\n";
+sleep(1);
+system('clear');
+echo "\e[0;31m╔╗──╔╗╔═══╗╔═╗─╔╗╔═══╗╔═══╗╔═══╗╔╗╔═╗
+║╚╗╔╝║║╔═╗║║║╚╗║║╚╗╔╗║║╔═╗║║╔═╗║║║║╔╝
+╚╗║║╔╝║║─║║║╔╗╚╝║─║║║║║║─║║║╚═╝║║╚╝╝─
+─║╚╝║─║╚═╝║║║╚╗║║─║║║║║╚═╝║║╔╗╔╝║╔╗║─
+─╚╗╔╝─║╔═╗║║║─║║║╔╝╚╝║║╔═╗║║║║╚╗║║║╚╗
+──╚╝──╚╝─╚╝╚╝─╚═╝╚═══╝╚╝─╚╝╚╝╚═╝╚╝╚═╝ . ERROR.404\n";
+echo "\e[0;34mSELAMAT DATANG DI TOOLS VANDARK.404\n";
+
+echo "\e[1;34mPERINGATAN!!! UNTUK HASIL YANG SEMPURNA INSTALL SEMUA COMMAND!!!\n";
+
+echo "\e[1;31m#============VANDARK.404============#\n";
+echo "\e[0;32m[1]INSTALL SCRIPT HACK FACEBOOK 1.7 (ANTI CHECKPOINT)\n";
+echo "\e[0;31m=====================================================\n";
+echo "\e[0;32m[2]INSTALL SCRIPT SPAM WHATSAPP 2019 (WORK 100%)\n";
+echo "\e[0;31m=====================================================\n";
+echo "\e[0;32m[3]INSTALL SCRIPT BOOM SPAM (WORK 100%)\n";
+echo "\e[0;31m=====================================================\n";
+echo "\e[0;32m[4]INSTALL SCRIPT KUMPULAN VIRTEX\n";
+echo "\e[0;31m=====================================================\n";
+echo "\e[0;32m[5]INSTALL SCRIPT VIRUS BERBENTUK APK FOR WHATSAPP\n";
+echo "\e[0;31m=====================================================\n";
+echo "\e[0;32m[6]EXIT\n";
+echo "\e[1;31m#===========MASUKAN PILIHAN==========#\n";
+
+echo "\e[0;36mMASUKAN PILIHAN BOSS KU : ";
+$menu = trim(fgets(STDIN));
+if ($menu == 1)
+{
+system( "git clone https://github.com/storiku/darkfb");
+}
+if ($menu == 2)
+{
+system( "git clone https://github.com/4L13199/LITESPAM");
+}
+if ($menu == 3)
+{
+system( "git clone https://github.com/Vanbom/bom");
+}
+if ($menu == 4)
+{
+system( "git clone https://github.com/MuhammadFathul/VIRTEX");
+}
+if ($menu == 5)
+{
+system( "git clone https://github.com/Mr-xDODOL/Malicious");
+}
+if ($menu == 6)
+{
+system( "exit");
+}
+
+}
+else
+{
+echo "\e[0;32mLOGIN GAGAL,JIKA TIDAK TAU USER SAMA PASS NYA HUBUNGI SAYA DI WHATSAPP 085700797860\n";
+}
+?>
